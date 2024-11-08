2bc8c2c467576b6a22598eccc38f7e4d2062bb07
Andrey Martinich <aimartinich@edu.hse.ru>
2024-11-08 18:36:56 +0300

    Added test dao

---
+++ backend/api_v1/users/dao.py
@@ -0,0 +1,67 @@
+from typing import Optional, List
+from sqlalchemy import select
+
+from core.dao import BaseDAO
+from api_v1.users.models_sql import User, UserProfile, UserInteraction
+from core.database_sql import async_session
+
+class UserDAO(BaseDAO[User]):
+    model = User
+
+    @classmethod
+    async def get_by_email(cls, email: str) -> Optional[User]:
+        async with async_session() as session:
+            query = select(cls.model).where(cls.model.email == email)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+    @classmethod
+    async def get_by_username(cls, username: str) -> Optional[User]:
+        async with async_session() as session:
+            query = select(cls.model).where(cls.model.username == username)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+class UserProfileDAO(BaseDAO[UserProfile]):
+    model = UserProfile
+
+    @classmethod
+    async def get_by_user_id(cls, user_id: int) -> Optional[UserProfile]:
+        async with async_session() as session:
+            query = select(cls.model).join(User).where(User.id == user_id)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+    @classmethod
+    async def update_weights(cls, profile_id: int, **weights) -> Optional[UserProfile]:
+        return await cls.update(profile_id, **weights)
+
+class UserInteractionDAO(BaseDAO[UserInteraction]):
+    model = UserInteraction
+
+    @classmethod
+    async def get_user_interactions(cls, user_id: int) -> List[UserInteraction]:
+        async with async_session() as session:
+            query = select(cls.model).where(
+                (cls.model.user_1_id == user_id) | 
+                (cls.model.user_2_id == user_id)
+            )
+            result = await session.execute(query)
+            return result.scalars().all()
+
+    @classmethod
+    async def create_interaction(
+        cls, 
+        user_1_id: int, 
+        user_2_id: int, 
+        game: str,
+        user_1_rating: int,
+        user_2_rating: int
+    ) -> UserInteraction:
+        return await cls.create(
+            user_1_id=user_1_id,
+            user_2_id=user_2_id,
+            game=game,
+            user_1_rating=user_1_rating,
+            user_2_rating=user_2_rating
+        ) 
---
+++ backend/core/dao.py
@@ -0,0 +1,45 @@
+from typing import Generic, TypeVar, Optional, List
+from sqlalchemy import select, update, delete
+
+from core.database_sql import Base, async_session
+
+ModelType = TypeVar('ModelType', bound=Base)
+
+class BaseDAO(Generic[ModelType]):
+    model = None
+
+    @classmethod
+    async def get_by_id(cls, model_id: int) -> Optional[ModelType]:
+        async with async_session() as session:
+            return await session.get(cls.model, model_id)
+
+    @classmethod
+    async def get_all(cls) -> List[ModelType]:
+        async with async_session() as session:
+            result = await session.execute(select(cls.model))
+            return result.scalars().all()
+
+    @classmethod
+    async def create(cls, **data) -> ModelType:
+        async with async_session() as session:
+            instance = cls.model(**data)
+            session.add(instance)
+            await session.commit()
+            await session.refresh(instance)
+            return instance
+
+    @classmethod
+    async def update(cls, model_id: int, **data) -> Optional[ModelType]:
+        async with async_session() as session:
+            query = update(cls.model).where(cls.model.id == model_id).values(**data)
+            await session.execute(query)
+            await session.commit()
+            return await cls.get_by_id(model_id)
+
+    @classmethod
+    async def delete(cls, model_id: int) -> bool:
+        async with async_session() as session:
+            query = delete(cls.model).where(cls.model.id == model_id)
+            result = await session.execute(query)
+            await session.commit()
+            return result.rowcount > 0 
