a0376937fc34692ae327d67f83947be563368120
Andrey Martinich <aimartinich@edu.hse.ru>
2024-11-19 21:30:24 +0300

    Monogo works

--- backend/api_v1/users/router.py
+++ backend/api_v1/users/router.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
+from backend.api_v1.games.models_nosql import MinecraftModel
 from backend.api_v1.users.dao import UserDAO, UserInteractionDAO
+from backend.core.database_mongo import MongoController
+from backend.api_v1.games.models_nosql import UserGamesModel
 
 
 user_router = APIRouter(prefix='/user', tags=['Users management'])
@@ -33,6 +36,14 @@ async def create_user_interaction(id1: int, id2: int, game: str, u1r: str, u2r: 
         return new_user_int
     except Exception as e:
         return {'status': str(e)}
+    
+@user_router.post('/create/ugm')
+async def create_ugm(hour: int):
+    game = MinecraftModel(hours_played=hour)
+    ugm = UserGamesModel(user_id=1, games=[game])
+    mongo = MongoController()
+    print(ugm.model_dump())
+    await mongo.add_user_games(ugm)
 
 
 @user_router.get('/me')
